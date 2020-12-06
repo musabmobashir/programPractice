b71419ba91553579d3804e14cecbb02dd2280c40
Musab Mobashir <musab.mobashir@hcl.com>
2020-12-06 00:18:43 -0500

    Updated, Fixed, and renamed singly LinkList

--- PracticeProject/src/datastructures/TestClass.java
+++ PracticeProject/src/datastructures/TestClass.java
@@ -5,50 +5,57 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		LinkList list1 = new LinkList();
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.add("abc");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		System.out.println("list1.getValueByIndex(1):" + list1.getValueByIndex(1));
+		SinglyLinkList singlyList1 = new SinglyLinkList();
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.add("abc");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		System.out.println("singlyList1.getValueByIndex(1):" + singlyList1.getValueByIndex(1));
 		try {
-			System.out.println("\"list1.getValueByIndex(2)\" - " + list1.getValueByIndex(2));
+			System.out.println("\"singlyList1.getValueByIndex(2)\" - " + singlyList1.getValueByIndex(2));
 		} catch (IndexOutOfBoundsException e) {
-			System.out.println("\"list1.getValueByIndex(2)\" - Error Message: " + e.getMessage());
+			System.out.println("\"singlyList1.getValueByIndex(2)\" - Error Message: " + e.getMessage());
 		}
-		list1.add("def");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.add("hij");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.add("klm");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.add("nop");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.add("qrs");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
+		singlyList1.add("def");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.add("hij");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.add("klm");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.add("nop");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.add("qrs");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
 		
-		list1.addFront("abc");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.addLast("yz");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		list1.addByIndex(8,"tuv");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
+		singlyList1.addFront("abc");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.addLast("yz");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		singlyList1.addByIndex(8,"tuv");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
 		
-		
-		list1.removeFirst();
-		System.out.println("\"list1.removeFirst()\"");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		
-		list1.removeLast();
-		System.out.println("\"list1.removeLast()\"");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
-		
-		list1.removeByIndex(2);
-		System.out.println("\"list1.removeByIndex(2)\"");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
+		System.out.println("\"singlyList1.indexOf(\"hij\")\" - " + singlyList1.indexOf("hij"));
+		System.out.println("\"singlyList1.indexOf(\"abc\")\" - " + singlyList1.indexOf("abc"));
+		System.out.println("\"singlyList1.indexOf(\"xyz\")\" - " + singlyList1.indexOf("xyz"));
 
-		list1.removeAll();
-		System.out.println("\"list1.removeAll()\"");
-		System.out.println("List1 - Size:" + list1.getSize() + " - List: " + list1.toString());
+		
+		singlyList1.removeFirst();
+		System.out.println("\"singlyList1.removeFirst()\"");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		
+		singlyList1.removeLast();
+		System.out.println("\"singlyList1.removeLast()\"");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+		
+		singlyList1.removeByIndex(2);
+		System.out.println("\"singlyList1.removeByIndex(2)\"");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+
+		singlyList1.removeAll();
+		System.out.println("\"singlyList1.removeAll()\"");
+		System.out.println("singlyList1 - Size:" + singlyList1.getSize() + " - List: " + singlyList1.toString());
+
+		System.out.println("\"singlyList1.indexOf(\"abc\")\" - " + singlyList1.indexOf("abc"));
+
 
 	}
 
