280a4b092ee6ab9697fd9b3151e2539a3f1a1374
Musab Mobashir <musab.mobashir@hcl.com>
2020-12-06 23:19:04 -0500

    Adding Testcases for unit Test for Singly LinkedList

--- PracticeProject/src/test/java/com/datastructures/linkedlist/SinglyLinkedListTest.java
+++ PracticeProject/src/test/java/com/datastructures/linkedlist/SinglyLinkedListTest.java
@@ -1,6 +1,9 @@
 package test.java.com.datastructures.linkedlist;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.*;
 
@@ -29,6 +32,145 @@ public class SinglyLinkedListTest {
 	@Test(expected = RuntimeException.class)
 	public void removeLastFromEmptyLinkList() {
 		singlyList1.removeLast();	
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void removeByIndexFromEmptyLinkList() {
+		singlyList1.removeByIndex(1);	
+	}
+	
+	@Test(expected = RuntimeException.class)
+	public void getValueByIndexFromEmptyLinkList() {
+		singlyList1.getValueByIndex(1);	
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void getValueByIndexOutOfIndexRange() {
+		singlyList1.add("abc");
+		singlyList1.add("qaz");
+		singlyList1.getValueByIndex(10);	
+	}
+	
+	@Test
+	public void getValueByIndex() {
+		singlyList1.add("abc");
+		singlyList1.add("qaz");
+		assertEquals("qaz", singlyList1.getValueByIndex(2));
+	}
+	
+	@Test
+	public void addToLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("abc");
+		assertEquals(1, singlyList1.getSize());
+		assertFalse(singlyList1.isEmpty());
+		assertEquals("abc", singlyList1.getValueByIndex(1));
+		singlyList1.add("qaz");
+		assertEquals(2, singlyList1.getSize());
+		assertEquals("qaz", singlyList1.getValueByIndex(2));
+	}
+	
+	@Test
+	public void addLastToLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.addLast("abc");
+		assertEquals(1, singlyList1.getSize());
+		assertFalse(singlyList1.isEmpty());
+		assertEquals("abc", singlyList1.getValueByIndex(1));
+		singlyList1.addLast("qaz");
+		assertEquals(2, singlyList1.getSize());
+		assertEquals("qaz", singlyList1.getValueByIndex(2));
+	}
+	
+	@Test
+	public void addFrontToLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("def");
+		singlyList1.add("ghi");
+		singlyList1.addFront("abc");
+		assertEquals(3, singlyList1.getSize());
+		assertFalse(singlyList1.isEmpty());
+		assertEquals("abc", singlyList1.getValueByIndex(1));
+	}
+	
+	@Test
+	public void addByIndexToLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("def");
+		singlyList1.add("jkl");
+		singlyList1.addByIndex(1,"abc");
+		assertEquals(3, singlyList1.getSize());
+		assertFalse(singlyList1.isEmpty());
+		assertEquals("abc", singlyList1.getValueByIndex(1));
+		singlyList1.addByIndex(3,"ghi");
+		assertEquals("ghi", singlyList1.getValueByIndex(3));
+		assertEquals("jkl", singlyList1.getValueByIndex(4));
+		assertEquals(4, singlyList1.getSize());
+		singlyList1.addByIndex(5,"mno");
+		assertEquals("mno", singlyList1.getValueByIndex(5));
+		assertEquals("jkl", singlyList1.getValueByIndex(4));
+		assertEquals(5, singlyList1.getSize());
+	}
+	@Test
+	public void removeForLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("def");
+		singlyList1.add("jkl");
+		singlyList1.addByIndex(1,"abc");
+		singlyList1.addByIndex(3,"ghi");
+		singlyList1.addByIndex(5,"mno");
+		singlyList1.removeFirst();
+		assertEquals("def", singlyList1.getValueByIndex(1));
+		singlyList1.removeLast();
+		assertEquals("jkl", singlyList1.getValueByIndex(3));
+		singlyList1.removeByIndex(2);
+		assertEquals(2, singlyList1.getSize());
+		assertEquals("jkl", singlyList1.getValueByIndex(2));
+		singlyList1.removeAll();
+		assertTrue(singlyList1.isEmpty());
+	}
+	
+	@Test
+	public void containsNodeForLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("abc");
+		singlyList1.add("def");
+		singlyList1.add("jkl");
+		assertTrue(singlyList1.contains("def"));
+	}
+	
+	@Test
+	public void indexOfNodeForEmptyLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		ArrayList<Integer> indexValues = new ArrayList<Integer>();
+		assertEquals(indexValues,singlyList1.indexOf("abc"));
+	}
+	
+	@Test
+	public void indexOfNodeForLinkedList_NodeDoesNotExist() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("abc");
+		singlyList1.add("def");
+		singlyList1.add("jkl");
+		singlyList1.add("abc");
+
+		ArrayList<Integer> indexValues = new ArrayList<Integer>();
+		assertEquals(indexValues,singlyList1.indexOf("xyz"));
+	}
+	
+	@Test
+	public void indexOfNodeForLinkedList() {
+		assertTrue(singlyList1.isEmpty());
+		singlyList1.add("abc");
+		singlyList1.add("def");
+		singlyList1.add("jkl");
+		singlyList1.add("abc");
+
+		ArrayList<Integer> indexValues = new ArrayList<Integer>();
+		indexValues.add(1);
+		indexValues.add(4);
+
+		assertEquals(indexValues,singlyList1.indexOf("abc"));
 	}
 	
 }
